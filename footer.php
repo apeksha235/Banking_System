ca7b76740e8f335c1701c6827956c572da573bcd
apeksha235 <84583787+apeksha235@users.noreply.github.com>
2021-10-12 22:33:52 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="footer">
+  <p>THIS bank</p>
+  <p>Made by- Apeksha Hegde</p>
+</div>
+</body>
+</html>
