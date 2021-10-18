b34c2b2eecabc31a0094aff052cfb806f819aad3
apeksha235 <84583787+apeksha235@users.noreply.github.com>
2021-10-18 22:42:50 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,23 @@
-
+<?php
+include 'includes/common.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="style1.css">
+<title>Sparks Bank</title>
+</head>
+<body>
+<div class="footer">
+  <p style="color: white; font-weight: bolder;">THIS bank</p>
+  <p style="color: white; font-weight: bolder;">Made by- Apeksha Hegde</p>
+</div>
+</body>
+</html>
