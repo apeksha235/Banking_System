fe60bf75f21ac6d52a0ee7a281bd71164acec935
apeksha235 <84583787+apeksha235@users.noreply.github.com>
2021-10-18 22:43:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,40 @@
+<?php
+include 'includes/common.php';
+?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Basic Banking System</title>
-
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <title>THIS Bank</title>
 </head>
+
 <body>
-
-<?php include('nav.php') ?>
-
+<?php
+include 'includes/header.php';
+?>
+<div class="content">
+    <div class="bg">
  <div class="container-fluid">
-      <h1 id="header" align="center"><marquee>Welcome to THIS bank!!</marquee></h1>
+      <h1 id="header" class="cssmarquee" align="center">Welcome to THIS bank!!</h1>
     <div id="abc">
-       <a href="all_users.php"><button class="button" id="red">View All Users</button></a>
-        <a href="transfer_money.php"><button class="button" id="blue">Transfer Money</button></a>
-       <a href="transactionhistory.php"><button class="button" id="green">View Transfer History</button></a>
+       <a href="transaction.php"><button class="button" id="red">View All Users</button></a>
+        <a href="transaction.php"><button class="button" id="blue">Transfer Money</button></a>
+       <a href="transaction_history.php"><button class="button" id="green">View Transfer History</button></a>
     </div>
    </div>
+  </div>
 </div>
-  <?php include('footer.php') ?>
+<?php
+include 'includes/footer.php';
+?>
 </body>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </html>
